08b917b2dff7e10a997b6d03ffa9c03c93919378
Prakash012b <148553683+Prakash012b@users.noreply.github.com>
2024-11-18 14:59:06 +0000

    set and get method
    setters and getters added for the instantiable classes

--- HealthApp/src/healthapp/MentalActivity.java
+++ HealthApp/src/healthapp/MentalActivity.java
@@ -9,5 +9,58 @@ package healthapp;
  * @author pyaku
  */
 public class MentalActivity {
+    private int id;
+    private String type;
+    private String duration;
+    private String caloriesBurned;
+
+    public MentalActivity(int id, String type,String duration, String caloriesBurned) {
+        this.id = id;
+        this.type = type;
+         this.duration = duration;
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public void setCaloriesBurned(String caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
+    
+    
+ 
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public String getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    
+         
+    
+    //compute method
+    
+    public void compute(){
+     
+}
     
 }
+
+
--- HealthApp/src/healthapp/MentalDiet.java
+++ HealthApp/src/healthapp/MentalDiet.java
@@ -9,5 +9,37 @@ package healthapp;
  * @author pyaku
  */
 public class MentalDiet {
+    private int id;
+    private String mealType;
+    private String calories;
+
+    public MentalDiet(int id, String mealType, String calories) {
+        this.id = id;
+        this.mealType = mealType;
+        this.calories = calories;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
+
+    public void setCalories(String calories) {
+        this.calories = calories;
+    }
+
+    public String getMealType() {
+        return mealType;
+    }
+
+    public String getCalories() {
+        return calories;
+    }
     
-}
+    public void compute(){
+    
+    }
+       }
